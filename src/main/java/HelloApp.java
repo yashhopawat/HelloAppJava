9dea0cdba70626351d877b6b8098623211595a3a
yashhopawat <yo9198@srmist.edu.in>
2026-03-18 09:28:02 +0530

    UC5

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,11 +1,13 @@
-public class HelloApp {
+pubpublic class HelloApp {
     public static void main(String[] args) {
 
+        
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
+            
             for (String name : args) {
-                System.out.println("Hello " + name);
+                System.out.println("Hello, " + name + "!");
             }
         }
     }
